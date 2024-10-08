79f11d81a8f8489cf33a65e341ac4c273d8ac388
obaid <72abokhald72@gmail.com>
2024-10-08 14:59:51 +0300

    No commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let img= document.getElementById("imagess");
+let back = document.querySelector(".container");
+function phons(phn){
+   img.src=phn;
+}
+function color(col){
+    back.style.background= col;
+}
